42ecc7e3ecaae2b3e82a48fb25c03cfa7dc84906
thamiscoder <thamirisoli34@gmail.com>
2025-03-26 21:40:31 -0300

    js01

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function login() {
-    let username = document.getElementById
+    const inputs = document.querySelectorAll('.input-box input');
+    const userName = inputs[0].value;
+    const passWord = inputs[1].value;
+
+    alert(`Your Username is ${userName} and your Password is ${passWord}`);
 }
